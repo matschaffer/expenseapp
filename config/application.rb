f3a9ded40c3a72735ef3cc30deeac09a04321284
Mat Schaffer <mat@schaffer.me>
2012-02-28 09:20:10 -0500

    Avoid heroku recompilation problems

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ end
 module Expenseapp
   class Application < Rails::Application
     config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
