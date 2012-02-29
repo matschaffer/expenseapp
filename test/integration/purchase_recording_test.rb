5efe8e9ab2541cdc1e44181bd28ffd433210d6b6
Mat Schaffer <mat@schaffer.me>
2012-02-29 07:57:22 -0500

    Login before performing test

--- test/integration/purchase_recording_test.rb
+++ test/integration/purchase_recording_test.rb
@@ -2,6 +2,17 @@ require 'test_helper'
 
 class PurchaseRecordingTest < ActionDispatch::IntegrationTest
   javascript
+
+  setup do
+    @mat = User.create(email: 'mat@schaffer.me',
+                       password: 'test1234',
+                       password_confirmation: 'test1234')
+    visit new_user_session_path
+    fill_in 'Email', with: 'mat@schaffer.me'
+    fill_in 'Password', with: 'test1234'
+    click_button 'Sign in'
+  end
+
   test "a new purchase" do
     visit purchases_path
     click_link "New Purchase"
