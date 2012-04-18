a6714ac8edd014fe620ccfc3bbba4a36ade28c15
Mat Schaffer <mat@schaffer.me>
2012-04-18 09:31:41 -0400

    Edit budgets from user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,7 @@ class UsersController < ApplicationController
   # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
+    @user.budgets.build
   end
 
   # POST /users
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :budgets_attributes
   belongs_to :household
   has_many :purchases
   has_many :budgets
+
+  accepts_nested_attributes_for :budgets
 
   def recent_household_purchases
     household.purchases.recent
